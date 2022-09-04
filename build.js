bbacdc43447931beddc3b22539dae9c73a074492
xijin.liu <xijin.liu@airbnb.com>
2022-09-05 01:52:29 +0800

    update

--- build.js
+++ build.js
@@ -14,6 +14,9 @@ htmlFiles.forEach(f => {
   console.log(String(s));
 });
 */
+const indexFile = path.join(__dirname, "index.html")
+const indexContent = fs.readFileSync(indexFile, 'utf-8');
+fs.writeFileSync(indexFile, indexContent.replace(/ver:\s+\d+/, "ver: " + Date.now()), 'utf-8');
 const s = exec(`cd ${__dirname} && ./node_modules/.bin/parcel build index.html --public-url '.' --out-dir docs`);
 console.log(String(s));
 
